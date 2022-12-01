3dd92425092946005aa3777cda31b53f964c5e90
nuygahoadong <nuyga28092002@gmail.com>
2022-12-01 22:31:21 +0700

    acas

--- Eshop-OP/app/Http/Controllers/InvoicesController.php
+++ Eshop-OP/app/Http/Controllers/InvoicesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Invoices;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class InvoicesController extends Controller
 {
     /**
@@ -26,6 +26,62 @@ class InvoicesController extends Controller
         $invoices = Invoices::where('Status', '=', 2,)->orderBy('id', 'DESC')->get();
         return view('admin.order.approvel', compact('invoices'));
     }
+
+
+   
+    public function search(){
+        // function convert_name($str) {
+        //     $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+        //     $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+        //     $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+        //     $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+        //     $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+        //     $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+        //     $str = preg_replace("/(đ)/", 'd', $str);
+        //     $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+        //     $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+        //     $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+        //     $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+        //     $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+        //     $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+        //     $str = preg_replace("/(Đ)/", 'D', $str);
+        //     $str = preg_replace("/(\“|\”|\,|\!|\&|\;|\@|\#|\%|\~|\`|\=|\_|\'|\]|\[|\}|\{|\)|\(|\+|\^)/", '-', $str);
+        //     $str = preg_replace("/( )/", '-', $str);
+        //     return strtolower($str);
+        // }
+        $categoryId=$_GET['categoryId'];
+        $search=$_GET['stringsrearch'];
+        if($categoryId==null){
+            if($search==null){
+                $fullrodutct=DB::table('Products as P')
+                ->select('P.id','P.name as NNa','P.Description','P.Price','P.Stock','B.Name','C.Name','P.Image')
+                ->join('Categorys as C','P.Category_id','=','C.id')
+                ->join('Brands as B','P.Brand_id','=','B.id')->get();
+                return $fullrodutct;
+            }
+             //$search="Điện";
+            $fullrodutct=DB::table('Products as P')
+            ->select('P.id','P.name as NNa','P.Description','P.Price','P.Stock','B.Name','C.Name','P.Image')
+            ->join('Categorys as C','P.Category_id','=','C.id')
+            ->join('Brands as B','P.Brand_id','=','B.id')
+            ->where('P.Name','LIKE','%'.$search.'%')
+            ->orWhere('C.Name','LIKE','%'.$search.'%')
+            ->orWhere('B.Name','LIKE','%'.$search.'%')
+            ->get();
+            return $fullrodutct;
+       
+        }
+        else{
+            $fullrodutct=DB::table('Products as P')
+            ->select('P.id','P.name as NNa','P.Description','P.Price','P.Stock','B.Name','C.Name','P.Image')
+            ->join('Categorys as C','P.Category_id','=','C.id')
+            ->join('Brands as B','P.Brand_id','=','B.id')
+            ->where('C.id',$categoryId)
+            ->get();
+            return $fullrodutct;
+        }
+        }
+    
 
     /**
      * Show the form for creating a new resource.
